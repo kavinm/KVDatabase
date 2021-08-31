669f8087a695671f0ecada96c05fff5fa8a3a01f
kavinm <kavin1810@gmail.com>
2021-08-31 19:50:42 -0400

    added checks to only flush once

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,52 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
-fn main() {
+ fn main() {
     //iterator skip first n elements, in this case it will skip kvstore
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
+    let key = arguments.next().expect("Key is not present");
     let value = arguments.next().unwrap();
     println!("The key is '{}' and the value is '{}' ", key, value);
 
-    let contents = format!("{}\t{}\n", key, value);
 
-    //returns a Result which is an enum, can be () which is unit, or Error
 
-    let KVpath = std::path::Path::new("kv.db");
 
-    if KVpath.exists(){
+    let kv_path = std::path::Path::new("kv.db");
 
+    if kv_path.exists(){
+        let mut database = Database::new().expect("Database::new() crashed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
+
+    match database.flush(){
+        Ok(()) => println!("Succesfully added key value pair to database"),
+        Err(err) => println!("Error: {}", err)
     }
+}
 
-    std::fs::write("kv.db", contents).unwrap();
+    
 
-    let database = Database::new().expect("Database::new() crashed");
+    
+
 
   
 } 
 
 
-//create the struct with fields
+//Database struct fields
 struct Database{
     map: HashMap<String, String>,
+    flush: bool
 }
 
-//this is where you have the methods
+//methods for Database
 impl Database{
 
     //function called new that returns a Database, new is not a keyword in rust
     fn new() -> Result<Database, std::io::Error>{
-        // read the kv.db file
 
-        // let contents = match std::fs::read_to_string("kv.db"){
-        //     Ok(c) => c,
-
-        //     Err(error) =>{
-        //         return Err(error);
-        //     }
-        // };
         let mut map = HashMap::new();
+
+        // read the kv.db file
         let contents = std::fs::read_to_string("kv.db")?;
         for line in contents.lines(){
             let (key, value ) = line.split_once('\t').expect("Corrupt database");
@@ -52,13 +54,53 @@ impl Database{
             //if you insert a key that already exists, it returns the existing key/value
             map.insert(key.to_owned(), value.to_owned());
         }
-        // parse the string
-        //populate our map  
-        Ok(Database{
-            map: map 
-        })
+
+        Ok(Database{map, flush: false})
     }
 
+    //method for tinserting into database
+    fn insert(&mut self, key: String, value: String){
+        self.map.insert(key, value);
+    }
+
+    //method for writing to database
+    fn flush(mut self) -> std::io::Result<()>{
+        self.flush = true;
+       do_flush(&self)
+    }
+
+}
+
+//called when a Database value goes out of scope and gets "dropped"
+impl Drop for Database{
+    fn drop(&mut self){
+        if !self.flush{
+            let _ = do_flush(self);
+        }
+        
+    }
+}
+
+fn do_flush(database: &Database) -> std::io::Result<()>{
+    println!("do flush is called");
+    let mut contents = String:: new();
+    for (key, value) in &database.map{
+
+
+        //less efficient code because not stored in a :
+
+        //let kvpair: String = format!("{}\t{}\n", key, value);
+        //contents.push_str(&kvpair);
+
+        contents.push_str(&key);
+        contents.push('\t');
+        contents.push_str(&value);
+        contents.push('\n');
+
+    }
+
+    std::fs::write("kv.db", contents)
+    
 }
 
 
